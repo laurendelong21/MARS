79303ceedcb37154528cd69a887dd6809b30440e
Lauren DeLong <l.n.delong@sms.ed.ac.uk>
2024-01-29 15:35:37 +0000

    Seeing how pw prob ratio changes

--- mycode/model/rules.py
+++ mycode/model/rules.py
@@ -66,6 +66,9 @@ def update_confs_piecewise(rule_dict, empirical_probs, alpha=0.1):
             old_conf = float(rule_dict[head][count][0])
             # get the piecewise probability
             pw_prob = piecewise_probability(mpath[2::], empirical_probs)
+            # de-bugging statements
+            if pw_prob != 0 and pw_prob < expected ** len(mpath[2::]):
+                print("PW prob is low")
             normed_prob = pw_prob / (expected ** len(mpath[2::]))  ## normalize it by the prob we expect
             # get the average of the new and old confidences
             adjustment = map_ratio_to_penalty(normed_prob, alpha)
