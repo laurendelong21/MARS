4ce4b730457b5f37f6522deb97e65fc325c2811b
Lauren Delong <s2200046@login04.ecdf.ed.ac.uk>
2024-03-19 11:08:39 +0000

    Adding configs files

---
+++ run_moanet_polo.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N moanet_polo
+#$ -cwd
+#$ -pe sharedmem 4
+#$ -l h_vmem=64G
+#$ -l h_rt=48:00:00
+#$ -M L.N.DELONG@sms.ed.ac.uk
+#$ -m beas
+#$ -o ./eddie_output/
+#$ -e ./eddie_output/
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# Load Python and corresponding env
+module load anaconda
+module load python/3.8.17
+source activate polo_env
+
+echo $(python --version)
+
+# Run the program
+./run.sh configs/MOA-net-polo.sh
+#./replicates.sh configs/MOA-net-polo.sh 5
+
